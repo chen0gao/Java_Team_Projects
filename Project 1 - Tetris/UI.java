e7fe3cae256d79409dad65ac42c244e8fb64b93f
yuhao-dong <73535417+yuhao-dong@users.noreply.github.com>
2021-06-01 20:16:56 -0400

    different colors

--- Project 1 - Tetris/UI.java
+++ Project 1 - Tetris/UI.java
@@ -33,16 +33,33 @@ public class UI {
             cube.drawString("Paused", init_x+window_w/2 - text_x_offset, init_y+window_h/2);
     		return;
     	}
-    	
-    	//Unpause Game
 
+    	// create our own colors
+    	Color color_1 = new Color(255,204,0); // orange
+        Color color_2 = new Color(230,230,0); // yellow
+        Color color_3 = new Color(102,255,153); // green
+        Color color_4 = new Color(102,140,255); // blue
+        Color color_5 = new Color(179,102,255); // violet
+        Color color_6 = new Color(255,0,0); // red
+
+    	//Unpause Game
         for (int row = 0; row < system.length; row++) {
             for (int col = 0; col < system[row].length; col++) {
                 // Different block color based on the value
                 if (system[row][col] == 0) {
                     cube.setColor(Color.WHITE);
-                } else if (system[row][col] == 1) {
-                    cube.setColor(Color.BLUE);
+                } else if (system[row][col] == 1 && Data.current_shape == 1) {
+                    cube.setColor(color_1);
+                } else if (system[row][col] == 1 && Data.current_shape == 2) {
+                    cube.setColor(color_2);
+                } else if (system[row][col] == 1 && Data.current_shape == 3) {
+                    cube.setColor(color_3);
+                } else if (system[row][col] == 1 && Data.current_shape == 4) {
+                    cube.setColor(color_4);
+                } else if (system[row][col] == 1 && Data.current_shape == 5) {
+                    cube.setColor(color_5);
+                } else if (system[row][col] == 1 && Data.current_shape == 6) {
+                    cube.setColor(color_6);
                 } else if (system[row][col] == 10) {
                     cube.setColor(Color.GRAY);
                 }
